6a4a822c6be3c4f44a6109c22cc354e4757f5b3f
bweinstein <brian.j.weinstein@colorado.edu>
2014-01-30 15:35:32 -0700

    Still working through bugs on 4. Still need to add more tests to 3

--- part4.rb
+++ part4.rb
@@ -1,3 +1,53 @@
 #Author: Brian Weinstein
 #Sources: rubydoc
 
+class WrongNumberOfPlayersError <  StandardError ; end
+
+class NoSuchStrategyError <  StandardError ; end
+
+ 
+
+def rps_game_winner(game)
+
+	#make sure game has 2 players
+    raise WrongNumberOfPlayersError unless game.length == 2
+	
+	#get R, P, S
+	@player1 = game[0][1] 
+	@player2 = game[1][1]
+
+	#if both players play same thing
+	if @player1 == "R" && @player2 == "R"
+		return game[1]
+	elseif @player1 == "S" && @player2 == "S"
+		return game[1]
+	elseif @player1 == "P" && @player2 == "P"
+		return game[1]
+	
+	#Player 1 wins
+	elseif @player1 == "R" && @player2 == "S"
+		return game[1]
+	elseif @player1 == "P" && @player2 == "R"
+		return game[1]
+	elseif @player[1] == "S" && @player2 == "P"
+		return game[1]
+	
+	#Player 2 wins
+	elseif @player1== "R" && @player2 == "P"
+		return game[2]
+	elseif @player1 == "P" && @player2 == "S"
+		return game[2]
+	elseif @player1 == "S" && @player2 == "P"
+		return game[2]
+	
+	else
+		NoSuchStrategyError
+	end
+	
+end
+#testing 
+
+	#testaargs = [ ["Brian", "R"], ["Julie", "P"] ] 
+	puts rps_game_winner([["Brian", "R"], ["Julie", "P"]]).to_s
+	
+
